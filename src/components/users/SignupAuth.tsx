dfe195ec0d4f50a24ad2dfc971626cf1b366cc7a
ejonghwan <sun87-1@daum.net>
2025-02-07 17:59:34 +0900

    인증 프론트 완료

--- src/components/users/SignupAuth.tsx
+++ src/components/users/SignupAuth.tsx
@@ -1,9 +1,17 @@
 "use client"
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { onUserAuthAPI } from '@/src/store/queryies/user/userQueryFn'
 import { useUserStore } from '@/src/store/front/user';
-
+import {
+	useRouter,
+	usePathname,
+	useSearchParams,
+	useSelectedLayoutSegment,
+	useSelectedLayoutSegments,
+	redirect,
+	notFound
+} from 'next/navigation'
 
 
 
@@ -19,8 +27,10 @@ const SignupAuth = () => {
     //     }
     //   });
 
+    const [ emailVerify, setEmailVerify ] = useState(false)
     const { authInfo } = useUserStore();
-
+    const router = useRouter()
+    
 
     useEffect(() => {
         // signupAuth()
@@ -29,13 +39,20 @@ const SignupAuth = () => {
         console.log('auth compo ??', authInfo)
     }, [authInfo])
 
-
-
-    const handleAuthComplate = async () => {
+    const handleEmailVerified = async () => {
 
         const aa = await onUserAuthAPI(authInfo)
-        console.log('fo res data 최종?', aa)
+        console.log('fo res data 최종?', aa.data.emailVerified)
+        setEmailVerify(aa.data.emailVerified)
     }
+
+    useEffect(() => {
+        if(emailVerify) {
+            alert('회원가입이 완료되었습니다.')
+            router.push('/')
+        }
+        
+    }, [emailVerify])
 
 
 
@@ -44,7 +61,17 @@ const SignupAuth = () => {
             <h2>메일로 인증메일이 전송되었습니다. 인증메일에서 인증을 완료하시고 인증완료 버튼을 눌러주세요</h2>
             {authInfo.email}<br />
             {authInfo.uid}
-            <button type='button' onClick={handleAuthComplate}>인증완료</button>
+            <button type='button' onClick={handleEmailVerified}>인증완료</button>
+
+            {emailVerify ? (
+                <div>인증이 완료되었습니다</div>
+            ) : (
+                <div>
+                    <strong>인증메일에서 인증을 완료해주세요.</strong>
+                    <p>인증메일에서 URL을 클릭하시면 인증이 완료됩니다</p>
+                </div>
+                
+            )}
         </div>
     )
 }

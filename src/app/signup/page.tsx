0a99da2dacb178bcf9e587887cc78dd16117b48e
ejonghwan <sun87-1@daum.net>
2025-03-13 00:28:02 +0900

    types test

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -13,6 +13,19 @@ const SignupPage = async () => {
     // console.log('fs?', path)
     // const htmlContent = fs.readFileSync(path.join(process.cwd(), 'public/test.html'), 'utf8')
 
+
+    // type test
+
+    // interface Type {
+    //     (n: number): { a: string; b: number, n: number }
+    // }
+    type Type = (n: number) => { a: string; b: number, n: number }
+    const hoho: Type = (n: number) => {
+        console.log('type test')
+        return { a: 'asd', b: 33, n: n }
+    }
+    hoho(3)
+
     return (
         <>
             {/* 외부 html test */}

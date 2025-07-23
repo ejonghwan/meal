dce32095d50c4bc299f0dad87a949f4f365a2777
ejonghwan <sun87-1@daum.net>
2025-07-23 18:53:22 +0900

    디테일  페이지 쿼리스트링 테스트

--- src/app/(beforeLogin)/restaurant/detail/[restaurantId]/page.tsx
+++ src/app/(beforeLogin)/restaurant/detail/[restaurantId]/page.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 
-const RestaurantDetailPage = ({ params }) => {
 
+interface Props {
+   params: { restaurantId: string }
+   // params: Promise<{ restaurantId: string }> // 얘는 promise 아닌듯 
+   searchParams: Promise<{ hoho: string }>
+}
+
+const RestaurantDetailPage = async ({ params, searchParams }: Props) => {
+
+
+   const { hoho } = await searchParams
+   console.log('searchParams', searchParams  , hoho)
 
    return (
-      <div>RestaurantDetailPage : {params.restaurantId}</div>
+      <>
+         <div>RestaurantDetailPage : {params.restaurantId}</div>
+         <div>RestaurantDetailPage : {hoho}</div>
+      </>
    )
 }
 
--- src/components/restaurant/restaurant-item.tsx
+++ src/components/restaurant/restaurant-item.tsx
@@ -100,7 +100,7 @@ const RestaurantItem = ({ restaurant }) => {
             <Divider className='h-[3px] mb-[15px] bg-bg100 mx-[-20px]' />
             <div className='flex flex-wrap items-center mb-[10px]'>
                 <div className='w-full flex items-center gap-[5px] justify-end'>
-                    <Link href={`/restaurant/detail/${restaurant.id}`} className='flex gap-[5px] items-center mb-[5px]'>
+                    <Link href={`/restaurant/detail/${restaurant.id}?hoho=zzzz`} className='flex gap-[5px] items-center mb-[5px]'>
                         <span className='text-[13px] text-[#d3d3d3]'>상세글 보기</span>
                         <span><PiArrowCircleRightDuotone /></span>
                     </Link>

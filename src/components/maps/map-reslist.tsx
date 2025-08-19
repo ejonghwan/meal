cf47ec8756eb15b688f3e8fdef526974891c46e8
ejonghwan <sun87-1@daum.net>
2025-08-19 23:56:27 +0900

    클러스터 테스트

--- src/components/maps/map-reslist.tsx
+++ src/components/maps/map-reslist.tsx
@@ -42,6 +42,26 @@ const MapResList = ({ address, restaurant, setMyRestaurantList, className }: Pro
          level: 3,
       });
 
+
+      // 클러스터 테스트 
+      var clusterer = new window.kakao.maps.MarkerClusterer({
+         map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
+         averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
+         minLevel: 10 // 클러스터 할 최소 지도 레벨 
+      });
+
+      console.log('clusterer?', clusterer)
+
+      // var markers = $(data.positions).map(function(i, position) {
+      //       return new kakao.maps.Marker({
+      //           position : new kakao.maps.LatLng(position.lat, position.lng)
+      //       });
+      //   });
+
+      //   // 클러스터러에 마커들을 추가합니다
+      //   clusterer.addMarkers(markers);
+
+
       // 장소 검색 객체를 생성합니다
       const ps = new window.kakao.maps.services.Places();
 

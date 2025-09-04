90eeb6ac4eb43de07a2184783a9b48418d56dc22
ejonghwan <sun87-1@daum.net>
2025-09-04 18:18:25 +0900

    백에서 주 지역도 같이 내려주기

--- src/components/maps/map-reslist.tsx
+++ src/components/maps/map-reslist.tsx
@@ -200,46 +200,9 @@ const MapResList = ({ address, restaurant, setMyRestaurantList, className }: Pro
          //   // 클러스터러에 마커들을 추가합니다
          clusterer.addMarkers(markers);
 
-         // const customOverlay = new window.kakao.maps.CustomOverlay({
-         //    map: map,
-         //    position: restaurant && new window.kakao.maps.LatLng(restaurant[0].mapInfo.y, restaurant[0].mapInfo.x),
-         //    content: `
-         //    <div class="customoverlay flex items-center gap-[6px] border border-1.5 border-primary bg-[#fff] p-[4px] pr-[8px]" style="min-width: 100px;  min-height: 11px; border-radius: 20px; position: relative; top: -9px; left: -0px; right:0;">
-
-         //       <div style="width: 22px; height: 22px; border-radius: 50%; flex: 0 0 auto;" class="bg-primary">
-         //          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 26" class="map_ico" fill="#fff">
-         //             <path fill-rule="evenodd" d="M5.5 9.307C5.5 7.205 7.069 5.5 9.004 5.5s3.504 1.705 3.504 3.807c0 1.818-1.171 3.334-2.738 3.714v7.48H8.368V13.05C6.738 12.724 5.5 11.174 5.5 9.307zm13.599.694V5.823H20.5V10c0 1.574-1.064 2.882-2.453 3.123V20.5h-1.4v-7.376c-1.388-.24-2.453-1.549-2.453-3.123V5.823h1.4V10c0 .73.444 1.343 1.052 1.558V5.823h1.401v5.736c.608-.215 1.052-.827 1.052-1.558z" clip-rule="evenodd">
-         //             </path>
-         //          </svg>
-         //       </div>
-
-         //       <div class="text-[#333] text-[12px] ">
-         //          <p class=" leading-none" style="">${restaurant && restaurant[0].mapInfo.name}</p>
-         //          <div class="flex items-center gap-[3px]" >
-         //             <div class="size-[8px] ">
-         //                <svg xmlns="http://www.w3.org/2000/svg" fill="#999" viewBox="0 0 13 13"><path d="M8.263 4.682h4.258a.479.479 0 0 1 .28.866L9.353 8.022l1.324 4.01a.477.477 0 0 1-.175.537.482.482 0 0 1-.563 0l-3.438-2.49-3.44 2.49a.48.48 0 0 1-.737-.538l1.324-4.01L.2 5.549a.477.477 0 0 1 .28-.867l4.258.001L6.044.675a.48.48 0 0 1 .912 0l1.307 4.007z"></path></svg>
-         //             </div>
-         //             <p class="text-[10px] mt-[2px] leading-none">${restaurant && restaurant[0].rating}</p>
-         //          </div>
-         //       </div>
-
-         //    </div>
-         // `,
-         //    yAnchor: 1
-         // });
-         // clusterer.addMarkers(customOverlay);
-
 
          map.setBounds(bounds);
 
-
-         // for (var i = 0; i < restaurant.length; i++) {
-         //    displayMarker(restaurant[i].mapInfo); //마커
-         //    bounds.extend(new window.kakao.maps.LatLng(restaurant[i].mapInfo.y, restaurant[i].mapInfo.x));
-
-         // }
-         // // 검색된 장소 위치를 기준으로 지도 범위를 재설정
-         // map.setBounds(bounds);
       }
 
       displayMyRestaurant(restaurant);
